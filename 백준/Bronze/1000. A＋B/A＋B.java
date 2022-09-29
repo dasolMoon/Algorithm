bb2e40c49e1240c9687ed9bc76e61e7fb36efeef
Dasol Moon <mds9852369@gmail.com>
2022-09-29 23:29:37 +0900

    [Bronze V] Title: A+B, Time: 200 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+ public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+     
+        int a = Integer.parseInt(scanner.next());
+        int b = Integer.parseInt(scanner.next());
+        
+        System.out.println(a+b);
+	}
+}
